98ca1af0d0b7016813a81e701505b5e45b3ce5ca
Craig Swank <craigswank@gmail.com>
2014-04-08 18:01:01 -0600

    added motor, going to test on a beaglbone

--- motor.go
+++ motor.go
@@ -5,17 +5,41 @@ import (
 )
 
 type Motor struct {
-	gpioA GPIO
-	gpioB GPIO
-	pwm   PWM
-	input GPIO
+	gpioA OutputDevice
+	gpioB OutputDevice
+	pwm   OutputDevice
+	poller InputDevice
 	status bool
+	started bool
 }
 
 func NewMotor(pin *Pin) (OutputDevice, error) {
-	var m *Motor
-	var err error
-	return m, err
+	p := pin.Pins["gpio_a"]
+	gpioA, err := NewGPIO(&p)
+	if err != nil {
+		return nil, err
+	}
+	p = pin.Pins["gpio_b"]
+	gpioB, err := NewGPIO(&p)
+	if err != nil {
+		return nil, err
+	}
+	p = pin.Pins["pwm"]
+	pwm, err := NewPWM(&p)
+	if err != nil {
+		return nil, err
+	}
+	p = pin.Pins["poller"]
+	poller, err := NewSwitch(&p)
+	if err != nil {
+		return nil, err
+	}
+	return &Motor{
+		gpioA: gpioA,
+		gpioB: gpioB,
+		pwm: pwm,
+		poller: poller,
+	}, nil
 }
 
 func (m *Motor) Update(msg *Message) {
@@ -23,6 +47,21 @@ func (m *Motor) Update(msg *Message) {
 }
 
 func (m *Motor) On(val *Value) error {
+	v, ok := val.Value.(float64)
+	if ! ok {
+		return nil
+	}
+	if v < 0.0 {
+		m.pwm.On(val)
+		m.gpioA.Off()
+		m.gpioB.On(nil)
+	} else if v > 0.0 {
+		m.pwm.On(val)
+		m.gpioA.On(nil)
+		m.gpioB.Off()
+	} else {
+		m.Off()
+	}
 	return nil
 }
 
@@ -31,5 +70,13 @@ func (m *Motor) Status() interface{} {
 }
 
 func (m *Motor) Off() error {
+	m.pwm.Off()
+	m.gpioA.On(nil)
+	m.gpioB.On(nil)
 	return nil
+}
+
+
+func (m *Motor) start() {
+	m.started = true
 }
