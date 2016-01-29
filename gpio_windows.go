9dbb234ea5eec868cdeec40750024e191d204594
Craig Swank <craigswank@gmail.com>
2016-01-29 22:50:23 +0000

    added gpio_windows.go so quimby can be compiled for windows

---
+++ gpio_windows.go
@@ -0,0 +1,37 @@
+package gogadgets
+
+type GPIO struct{}
+
+func NewGPIO(pin *Pin) (OutputDevice, error) {
+	return nil, nil
+}
+
+func (g *GPIO) Config() ConfigHelper {
+	return ConfigHelper{}
+}
+
+func (g *GPIO) Init() error {
+	return nil
+}
+
+func (g *GPIO) Update(msg *Message) {}
+
+func (g *GPIO) On(val *Value) error {
+	return nil
+}
+
+func (g *GPIO) Status() interface{} {
+	return 0
+}
+
+func (g *GPIO) Off() error {
+	return nil
+}
+
+func (g *GPIO) writeValue(path, value string) error {
+	return nil
+}
+
+func (g *GPIO) Wait() (bool, error) {
+	return false, nil
+}
